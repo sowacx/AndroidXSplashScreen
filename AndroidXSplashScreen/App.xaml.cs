b6b7eac61827b74c3e589c363eee4621f9fe18fa
Michal Witczak <mhiz@demant.com>
2023-07-10 10:37:24 +0200

    init

---
+++ AndroidXSplashScreen/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AndroidXSplashScreen;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ AndroidXSplashScreen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AndroidXSplashScreen;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
